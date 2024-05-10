87a0de287c4b78675d1a1569f0b7e1ee463e6ca4
CarmSC <carmencabrera2035@gmail.com>
2024-05-10 15:34:46 +0200

    Update class

--- src/main/java/com/carmen/springboot/app/aop/springbootaop/aop/GreetingAop.java
+++ src/main/java/com/carmen/springboot/app/aop/springbootaop/aop/GreetingAop.java
@@ -3,9 +3,11 @@ package com.carmen.springboot.app.aop.springbootaop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -52,6 +54,28 @@ public class GreetingAop {
           String args = Arrays.toString(joinPoint.getArgs()); //Convertir un array en string
           logger.info(" Despues de lanzar la excepcion : " + method + " con los argumentos  " + args);
 
-    }
+      }
+
+      @Around("execution(* com.carmen.springboot.app.aop.springbootaop.services.*.*(..))")
+      public Object loggerAround(ProceedingJoinPoint  joinPoint) throws Throwable{
+            String method = joinPoint.getSignature().getName();
+            String args = Arrays.toString(joinPoint.getArgs());
+
+            Object result = null;
+
+            try {
+                  logger.info("El metodo: " + method + "() con los parametros" + args);
+                  result = joinPoint.proceed();
+                  logger.info("El metodo: " + method + "() Retorna el resultado:" + result);
+                  return result;
+                  
+            } catch (Throwable e) {
+                  logger.error("Error en la llamada del metodo : " + method + "()");
+                  throw e;
+            }
+
+            //return result;
+      }
+
 
 }
--- src/main/java/com/carmen/springboot/app/aop/springbootaop/services/GreetingService.java
+++ src/main/java/com/carmen/springboot/app/aop/springbootaop/services/GreetingService.java
@@ -3,5 +3,6 @@ package com.carmen.springboot.app.aop.springbootaop.services;
 public interface GreetingService {
 
       String sayHello(String person , String phrase);
+      String sayHelloError(String person , String phrase);
 
 }
